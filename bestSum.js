fba238c0b2d456cbc2f9a5f2bd1baa6e9bd4fea5
unknown <pedro@tecnocarcuritiba.com.br>
2024-07-03 13:08:08 -0300

    Added bestSum method

---
+++ bestSum.js
@@ -0,0 +1,22 @@
+const cache = new Map()
+
+const bestSum = (target, arr) => {
+    if(cache.has(target)) return cache.get(target)
+    if (target === 0) return []
+    if (target < 0) return null
+    
+    let shortest = null
+    
+    for (let num of arr){
+        const decreasedTarget = target - num
+        const result = bestSum(decreasedTarget, arr)
+        
+        if(result !== null){
+            const combnation = [...result, num]
+            cache.set(target,combnation)
+            if(shortest === null || combnation.length < shortest.length) shortest = combnation
+        }
+    }    
+    return shortest
+}
+console.log(bestSum(300,[1,2,3,4,5]))
